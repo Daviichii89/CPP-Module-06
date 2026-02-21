896cef24f64fedf6386d96e46417f534973b8e2b
david.fernandez <david.fernandez@manomano.com>
2026-02-21 09:34:37 +0100

    finished ex01

---
+++ ex01/main.cpp
@@ -0,0 +1,26 @@
+#include "Serializer.hpp"
+#include <iostream>
+
+int main()
+{
+	Data data;
+	data.value = 42;
+	data.name = "test";
+
+	std::cout << "Original Data: value=" << data.value << ", name=\"" << data.name << "\"" << std::endl;
+	std::cout << "Address of data: " << &data << std::endl;
+
+	uintptr_t raw = Serializer::serialize(&data);
+	std::cout << "Serialized (uintptr_t): " << raw << std::endl;
+
+	Data* ptr = Serializer::deserialize(raw);
+	std::cout << "Deserialized pointer: " << ptr << std::endl;
+	std::cout << "Deserialized Data: value=" << ptr->value << ", name=\"" << ptr->name << "\"" << std::endl;
+
+	if (ptr == &data)
+		std::cout << "OK: deserialize(serialize(ptr)) == ptr" << std::endl;
+	else
+		std::cout << "ERROR: pointers differ!" << std::endl;
+
+	return 0;
+}
