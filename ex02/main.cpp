918179ef8fa2d808c711da8dbe45cec369ddad2b
david.fernandez <david.fernandez@manomano.com>
2026-02-21 10:03:58 +0100

    finished ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,77 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+Base* generate(void)
+{
+	int r = rand() % 3;
+	if (r == 0)
+		return new A();
+	if (r == 1)
+		return new B();
+	return new C();
+}
+
+void identify(Base* p)
+{
+	if (dynamic_cast<A*>(p))
+	{
+		std::cout << "A" << std::endl;
+		return;
+	}
+	if (dynamic_cast<B*>(p))
+	{
+		std::cout << "B" << std::endl;
+		return;
+	}
+	if (dynamic_cast<C*>(p))
+	{
+		std::cout << "C" << std::endl;
+		return;
+	}
+}
+
+void identify(Base& r)
+{
+	try
+	{
+		(void)dynamic_cast<A&>(r);
+		std::cout << "A" << std::endl;
+		return;
+	}
+	catch (std::bad_cast&) {}
+	try
+	{
+		(void)dynamic_cast<B&>(r);
+		std::cout << "B" << std::endl;
+		return;
+	}
+	catch (std::bad_cast&) {}
+	try
+	{
+		(void)dynamic_cast<C&>(r);
+		std::cout << "C" << std::endl;
+		return;
+	}
+	catch (std::bad_cast&) {}
+}
+
+int main()
+{
+	srand(time(NULL));
+	for (int i = 0; i < 5; i++)
+	{
+		Base* p = generate();
+		std::cout << "Pointer:   ";
+		identify(p);
+		std::cout << "Reference: ";
+		identify(*p);
+		delete p;
+		std::cout << std::endl;
+	}
+	return 0;
+}
